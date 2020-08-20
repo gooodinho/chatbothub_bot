5b48e2ee89bf6d339cc68701748b03338bbbd4fb
gooodinho <andrey09132000@gmail.com>
2020-08-20 03:12:11 +0300

    finish ordering process

--- handlers/users/order_page_buttons.py
+++ handlers/users/order_page_buttons.py
@@ -1,7 +1,6 @@
 from aiogram import types
 from loader import dp, bot
 from keyboards.inline.contact_menu import contact_menu
-from keyboards.default.order_menu import order_menu
 from aiogram.types import CallbackQuery
 from keyboards.inline.contact_callback import contact_callback
 from aiogram.dispatcher import FSMContext
@@ -9,9 +8,9 @@ from utils.notify_admins import order_notify
 from keyboards.inline.cancel import cancel_menu
 
 
-@dp.callback_query_handler(text="cancel_action")
+@dp.callback_query_handler(text="cancel_action", state="send_contact")
 async def cancel_action(call: CallbackQuery, state: FSMContext):
-    print("FINISH STATE")
+    await call.answer(cache_time=10)
     await state.finish()
     await call.message.delete()
     await bot.send_message(chat_id=call.message.chat.id, text="Действие отменено")
@@ -32,13 +31,18 @@ async def get_email(call: CallbackQuery, state: FSMContext):
     await call.answer(cache_time=10)
     await call.message.edit_text("<b>Введите Вашу почту:</b> ")
     await call.message.edit_reply_markup(cancel_menu)
-    await state.set_state('email_address')
+    await state.set_state('send_contact')
+    msg = call.message
+    await state.update_data(msg=msg)
 
 
-@dp.message_handler(state="email_address")
+@dp.message_handler(state="send_contact")
 async def send_email(message: types.Message, state: FSMContext):
-    await message.answer("В ближайшее время мы с Вами свяжимся, ожидайте")
     await order_notify(dp, text=f"Связь - {state} - {message.text}")
+    data = await state.get_data()
+    msg = data.get("msg")
+    await msg.delete()
+    await message.answer("В ближайшее время мы с Вами свяжимся, ожидайте")
     await state.finish()
 
 
@@ -46,39 +50,27 @@ async def send_email(message: types.Message, state: FSMContext):
 async def get_telegram(call: CallbackQuery, state: FSMContext):
     await call.answer(cache_time=1)
     await call.message.edit_text("<b>Введите Ваш никнейм или номер телефона:</b> ")
-    await state.set_state('telegram')
-
-
-@dp.message_handler(state="telegram")
-async def send_telegram(message: types.Message, state: FSMContext):
-    await message.answer("В ближайшее время мы с Вами свяжимся, ожидайте")
-    await order_notify(dp, text=f"Связь - {state} - {message.text}")
-    await state.finish()
+    await call.message.edit_reply_markup(cancel_menu)
+    await state.set_state('send_contact')
+    msg = call.message
+    await state.update_data(msg=msg)
 
 
 @dp.callback_query_handler(contact_callback.filter(type="viber"))
 async def get_viber(call: CallbackQuery, state: FSMContext):
     await call.answer(cache_time=1)
     await call.message.edit_text("<b>Введите Ваш номер телефона:</b> ")
-    await state.set_state('viber')
-
-
-@dp.message_handler(state="viber")
-async def send_viber(message: types.Message, state: FSMContext):
-    await message.answer("В ближайшее время мы с Вами свяжимся, ожидайте")
-    await order_notify(dp, text=f"Связь - {state} - {message.text}")
-    await state.finish()
+    await call.message.edit_reply_markup(cancel_menu)
+    await state.set_state('send_contact')
+    msg = call.message
+    await state.update_data(msg=msg)
 
 
 @dp.callback_query_handler(contact_callback.filter(type="phone"))
 async def get_phone(call: CallbackQuery, state: FSMContext):
     await call.answer(cache_time=1)
     await call.message.edit_text("<b>Введите Ваш номер телефона:</b> ")
-    await state.set_state('phone')
-
-
-@dp.message_handler(state="phone")
-async def send_phone(message: types.Message, state: FSMContext):
-    await message.answer("В ближайшее время мы с Вами свяжимся, ожидайте")
-    await order_notify(dp, text=f"Связь - {state} - {message.text}")
-    await state.finish()
+    await call.message.edit_reply_markup(cancel_menu)
+    await state.set_state('send_contact')
+    msg = call.message
+    await state.update_data(msg=msg)
